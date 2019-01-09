b6b6d2a97bfe0159638d82408945527b58db9784
TianchengLee <ltc6634284@gmail.com>
2019-01-09 20:50:36 +0800

    渲染列表

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+class TodoInfo {
+  id: number
+  content: string
+  isDone: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todomvc';
+  public todos: Array<TodoInfo> = [
+    { id: 1, content: '哈哈', isDone: true },
+    { id: 2, content: '嘿嘿', isDone: false },
+    { id: 3, content: '嘎嘎', isDone: false },
+  ]
 }
